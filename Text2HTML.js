13db60419292e4f8a7565f072dee29aa8a061ce8
TheColaber <72760579+TheColaber@users.noreply.github.com>
2020-11-24 15:25:22 -0500

    Remove Comments and make shorter

--- Text2HTML.js
+++ Text2HTML.js
@@ -1,87 +1,73 @@
 Element.prototype.insertHTML = function (text) {
   function analyzeElement(placement, modtext) {
     let i = 0;
-    while (modtext.charCodeAt(i) == 32 || modtext.charCodeAt(i) == 10) { i++ }
+    while (modtext.charCodeAt(i) == 32 || modtext.charCodeAt(i) == 10) i++;
     if (i == modtext.length) return;
-    if (modtext.charAt(i) == "<") { // This is an element/comment
-      if (modtext.substring(i, i+4) == "<!--") { // Element is comment?
-        i += 4
-        let commentMessage = ""
-        while (modtext.substring(i, i+3) != "-->") { // We will find what was written in the comment
-          commentMessage += modtext.charAt(i)
-          i++
+    if (modtext.charAt(i) == "<") {
+      if (modtext.substring(i, i+4) == "<!--") {
+        i += 4;
+        let commentMessage = "";
+        while (modtext.substring(i, i+3) != "-->") {
+          commentMessage += modtext.charAt(i);
+          i++;
         }
         var comment = document.createComment(commentMessage);
         placement.appendChild(comment);
-        analyzeElement(placement, modtext.substring(i + 3, modtext.length)) // Let's analyze our siblings
+        analyzeElement(placement, modtext.substring(i + 3, modtext.length));
         return;
       }
-      let selfclosing = 0
+      let selfclosing = 0;
       let tagName = "";
-      i++
-      while (!(modtext.charAt(i) == ">" || modtext.charAt(i) == " " || modtext.substring(i, i+2) == "/>")) { // Find tag name
-        tagName += modtext.charAt(i)
-        i++
-      }
-      try {
-        let element = placement.appendChild(document.createElement(tagName)) // Create an element to append
-      } catch (e) {
-        throw `Error in creating element "${tagName}"`
-      }
+      i++;
+      for (; !(modtext.charAt(i) == ">" || modtext.charAt(i) == " " || modtext.substring(i, i+2) == "/>"); i++) tagName += modtext.charAt(i);
+      try { var element = placement.appendChild(document.createElement(tagName)) }
+      catch { throw `Error in creating element "${tagName}"` }
       selfclosing+= !element.outerHTML.includes("</");
-      while (modtext.charAt(i) != ">") { // Repeat till end of starting tag
-        while (modtext.charAt(i) == " ") { i++ }
+      while (modtext.charAt(i) != ">") {
+        while (modtext.charAt(i) == " ") i++;
         if (modtext.substring(i, i+2) == "/>") {
-          selfclosing++
+          selfclosing++;
           break;
         }
-        let attributeName = ""
-        while (modtext.charAt(i) != "=") { // Find attribute name
-          attributeName += modtext.charAt(i)
-          i++
-        }
-        i++
-        let quoteType = modtext.charAt(i); // Get the type of quote the user was using to start
-        i++
-        let attributeValue = ""
-        while (modtext.charAt(i) != quoteType) { // Get attribute value
-          attributeValue += modtext.charAt(i)
-          i++
-        }
-        element.setAttribute(attributeName, attributeValue) // Set these atrributes to the element appended
-        i++
+        let attributeName = "";
+        for (; modtext.charAt(i) != "="; i++) attributeName += modtext.charAt(i);
+        i++;
+        let quoteType = modtext.charAt(i);
+        i++;
+        let attributeValue = "";
+        for (; modtext.charAt(i) != quoteType; i++) attributeValue += modtext.charAt(i);
+        element.setAttribute(attributeName, attributeValue);
+        i++;
       }
       let j = i + selfclosing;
       if (!selfclosing) {
         j = i
         let duplicates = 0;
-        while (j < modtext.length) {
-          if (modtext.substring(j, j + `<${tagName}`.length) == `<${tagName}`) duplicates++
-          if (modtext.substring(j, j + `</${tagName}>`.length) == `</${tagName}>`) { // Found Ends of duplicates? We can reduse the variable
-            duplicates--
+        for (; j < modtext.length; j++) {
+          if (modtext.substring(j, j + `<${tagName}`.length) == `<${tagName}`) duplicates++;
+          if (modtext.substring(j, j + `</${tagName}>`.length) == `</${tagName}>`) {
+            duplicates--;
             if (duplicates == -1) break;
           }
-          j++
         }
-        analyzeElement(element, modtext.substring(i+1, j)) // Let's analyze our inner
-        analyzeElement(element.parentElement, modtext.substring(j + `</${tagName}>`.length, modtext.length)) // Let's analyze our siblings
-      } else analyzeElement(element.parentElement, modtext.substring(j, modtext.length)) // Let's analyze our siblings
+        analyzeElement(element, modtext.substring(i+1, j));
+        analyzeElement(element.parentElement, modtext.substring(j + `</${tagName}>`.length, modtext.length));
+      } else analyzeElement(element.parentElement, modtext.substring(j, modtext.length));
     }
-    else { // This is a text node
+    else {
       let j = i;
       let innerText = "";
-      while (!(modtext.charCodeAt(j) == 10 || j == modtext.length)) {
+      for (; !(modtext.charCodeAt(j) == 10 || j == modtext.length); j++) {
         if (modtext.charAt(j) == "<" && modtext.substring(j, modtext.length).match(/<.*>/g).length) break;
         innerText += modtext.charAt(j);
-        j++;
       }
-      if (placement.childNodes.length) { // Does our parent have any nodes already? If so let's create a text node
-        let textnode = document.createTextNode(innerText)
-        placement.append(textnode)
+      if (placement.childNodes.length) {
+        let textnode = document.createTextNode(innerText);
+        placement.append(textnode);
       } else placement.innerText = innerText;
-      analyzeElement(placement, modtext.substring(j, modtext.length))
+      analyzeElement(placement, modtext.substring(j, modtext.length));
     }
   }
-  analyzeElement(this, text)
+  analyzeElement(this, text);
   return this;
 }
